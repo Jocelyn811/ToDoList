19c4266b157b07a01628028d1a409df56bdde202
Jocelyn811 <1799162496@qq.com>
2020-09-22 19:37:55 +0800

    img

---
+++ script.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+    function getNowDate(){
+        var date= new Date();
+        var year=date.getFullYear();
+        var month=date.getMonth();
+        var day=date.getDate();
+        var hour=date.getHours();
+        var minute=date.getMinutes();
+        var second=date.getSeconds();
+        var weekArr=date.getFullYear();
+
+    }
+</script>
